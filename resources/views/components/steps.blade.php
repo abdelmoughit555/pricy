27027ea67b41363f70a64e84a0888a63e589aaad
abdelmoughit555 <abdelmoughit.fouham@promy.fr>
2021-03-15 17:28:56 +0100

    finish layouts

---
+++ resources/views/components/steps.blade.php
@@ -0,0 +1,322 @@
+<div class="container flex flex-col justify-center w-5/12 mx-auto">
+    <!--step 1 -->
+    <div>
+        <div>
+            <p class="font-semibold text-pricy-base text-pricy-gray-400">Step 1/3 :</p>
+        </div>
+        <div class="p-12 mt-8 rounded-lg bg-pricy-gray-100" style="height: 394px">
+            <h2 class="font-semibold text-gray-800 text-pricy-base">Whish producy would you like to test?</h2>
+            <div class="relative mt-8">
+                <input type="text"
+                    class="w-full px-8 py-3 text-sm placeholder-gray-600 rounded-lg outline-none text-pricy-gray-400"
+                    placeholder="Search Product">
+                <div class="absolute left-2 top-3">
+                    <svg width="20" height="20" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M9.17515 15.6835C9.52625 15.3324 9.52625 14.7631 9.17515 14.412C8.82404 14.0609 8.25478 14.0609 7.90367 14.412L4.3074 18.0083C3.95629 18.3594 3.95629 18.9287 4.3074 19.2798C4.65851 19.6309 5.22777 19.6309 5.57888 19.2798L9.17515 15.6835Z"
+                            fill="#1B2124" />
+                        <path
+                            d="M12.1362 15.0478C9.65351 15.0478 7.64087 13.0352 7.64087 10.5525C7.64087 8.0698 9.65351 6.05717 12.1362 6.05717C14.6189 6.05717 16.6316 8.0698 16.6316 10.5525C16.6316 13.0352 14.6189 15.0478 12.1362 15.0478ZM12.1362 16.846C15.612 16.846 18.4297 14.0283 18.4297 10.5525C18.4297 7.07672 15.612 4.25903 12.1362 4.25903C8.66042 4.25903 5.84274 7.07672 5.84274 10.5525C5.84274 14.0283 8.66042 16.846 12.1362 16.846Z"
+                            fill="#1B2124" />
+                    </svg>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--step 2 -->
+    <div class="mt-6">
+        <div>
+            <p class="font-semibold text-pricy-base text-pricy-gray-400">Step 2/3 :</p>
+        </div>
+        <div class="mt-12">
+            <div>
+                <div class="grid grid-cols-4 gap-x-12">
+                    <div>
+                        <p class="text-sm font-medium text-pricy-gray-400">Variants</p>
+                    </div>
+                    <div>
+                        <p class="text-sm font-medium text-pricy-gray-300">Split Cycle</p>
+                    </div>
+                    <div>
+                        <p class="text-sm font-medium text-pricy-gray-300">variant A</p>
+                    </div>
+                    <div>
+                        <p class="text-sm font-medium text-pricy-gray-300">variant B</p>
+                    </div>
+                </div>
+                <!--split 1 -->
+                <div class="grid items-center grid-cols-4 mt-8 gap-x-12">
+                    <div>
+                        <p class="text-sm font-medium text-pricy-gray-200">Current price</p>
+                    </div>
+                    <div class="relative">
+                        <select name=""
+                            class="w-full h-10 px-2 text-sm font-medium text-gray-700 border rounded outline-none appearance-none "
+                            id="">
+                            <option value="3 days">3 days</option>
+                        </select>
+                        <div class="absolute top-4 right-4">
+                            <svg width="8" height="6" viewBox="0 0 8 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M3.29048 5.51685C3.63972 6.00078 4.36028 6.00078 4.70952 5.51685L7.32911 1.88706C7.74674 1.30836 7.33324 0.5 6.61958 0.5H1.38042C0.666761 0.5 0.253257 1.30836 0.670893 1.88705L3.29048 5.51685Z"
+                                    fill="#484A4F" fill-opacity="0.47" />
+                            </svg>
+
+                        </div>
+                    </div>
+                    <div class="flex items-center h-10 space-x-2">
+                        <div class="flex items-center h-full pl-3 space-x-2 border rounded">
+                            <div>
+                                <p class="text-sm uppercase font-mediumtext-gray-700">
+                                    MAD
+                                </p>
+                            </div>
+                            <div class="w-full h-full">
+                                <input type="text" class="w-full h-full text-sm font-medium text-gray-700 outline-none"
+                                    value="23.9">
+
+                            </div>
+                        </div>
+                        <div class="flex flex-col space-y-2">
+                            <div>
+                                <svg width="8" height="6" viewBox="0 0 8 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M3.29048 0.49755C3.63972 0.0136235 4.36028 0.0136232 4.70952 0.49755L7.32911 4.12735C7.74674 4.70604 7.33324 5.5144 6.61958 5.5144H1.38042C0.666761 5.5144 0.253257 4.70604 0.670893 4.12735L3.29048 0.49755Z"
+                                        fill="#484A4F" />
+                                </svg>
+
+                            </div>
+                            <div>
+                                <svg width="8" height="6" viewBox="0 0 8 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M3.29048 5.51685C3.63972 6.00078 4.36028 6.00078 4.70952 5.51685L7.32911 1.88706C7.74674 1.30836 7.33324 0.5 6.61958 0.5H1.38042C0.666761 0.5 0.253257 1.30836 0.670893 1.88705L3.29048 5.51685Z"
+                                        fill="#484A4F" fill-opacity="0.47" />
+                                </svg>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex items-center h-10 space-x-2">
+                        <div class="flex items-center h-full pl-3 space-x-2 border rounded">
+                            <div>
+                                <p class="text-sm uppercase font-mediumtext-gray-700">
+                                    MAD
+                                </p>
+                            </div>
+                            <div class="w-full h-full">
+                                <input type="text" class="w-full h-full text-sm font-medium text-gray-700 outline-none"
+                                    value="23.9">
+
+                            </div>
+                        </div>
+                        <div class="flex flex-col space-y-2">
+                            <div>
+                                <svg width="8" height="6" viewBox="0 0 8 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M3.29048 0.49755C3.63972 0.0136235 4.36028 0.0136232 4.70952 0.49755L7.32911 4.12735C7.74674 4.70604 7.33324 5.5144 6.61958 5.5144H1.38042C0.666761 5.5144 0.253257 4.70604 0.670893 4.12735L3.29048 0.49755Z"
+                                        fill="#484A4F" />
+                                </svg>
+
+                            </div>
+                            <div>
+                                <svg width="8" height="6" viewBox="0 0 8 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M3.29048 5.51685C3.63972 6.00078 4.36028 6.00078 4.70952 5.51685L7.32911 1.88706C7.74674 1.30836 7.33324 0.5 6.61958 0.5H1.38042C0.666761 0.5 0.253257 1.30836 0.670893 1.88705L3.29048 5.51685Z"
+                                        fill="#484A4F" fill-opacity="0.47" />
+                                </svg>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--split 2 -->
+                <div class="grid items-center grid-cols-4 mt-8 gap-x-12">
+                    <div>
+                        <p class="text-sm font-medium text-pricy-gray-200">Split 2</p>
+                    </div>
+                    <div class="relative">
+                        <select name=""
+                            class="w-full h-10 px-2 text-sm font-medium text-gray-700 border rounded outline-none appearance-none "
+                            id="">
+                            <option value="3 days">3 days</option>
+                        </select>
+                        <div class="absolute top-4 right-4">
+                            <svg width="8" height="6" viewBox="0 0 8 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M3.29048 5.51685C3.63972 6.00078 4.36028 6.00078 4.70952 5.51685L7.32911 1.88706C7.74674 1.30836 7.33324 0.5 6.61958 0.5H1.38042C0.666761 0.5 0.253257 1.30836 0.670893 1.88705L3.29048 5.51685Z"
+                                    fill="#484A4F" fill-opacity="0.47" />
+                            </svg>
+
+                        </div>
+                    </div>
+                    <div class="flex items-center h-10 space-x-2">
+                        <div class="flex items-center h-full pl-3 space-x-2 border rounded">
+                            <div>
+                                <p class="text-sm uppercase font-mediumtext-gray-700">
+                                    MAD
+                                </p>
+                            </div>
+                            <div class="w-full h-full">
+                                <input type="text" class="w-full h-full text-sm font-medium text-gray-700 outline-none"
+                                    value="23.9">
+
+                            </div>
+                        </div>
+                        <div class="flex flex-col space-y-2">
+                            <div>
+                                <svg width="8" height="6" viewBox="0 0 8 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M3.29048 0.49755C3.63972 0.0136235 4.36028 0.0136232 4.70952 0.49755L7.32911 4.12735C7.74674 4.70604 7.33324 5.5144 6.61958 5.5144H1.38042C0.666761 5.5144 0.253257 4.70604 0.670893 4.12735L3.29048 0.49755Z"
+                                        fill="#484A4F" />
+                                </svg>
+
+                            </div>
+                            <div>
+                                <svg width="8" height="6" viewBox="0 0 8 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M3.29048 5.51685C3.63972 6.00078 4.36028 6.00078 4.70952 5.51685L7.32911 1.88706C7.74674 1.30836 7.33324 0.5 6.61958 0.5H1.38042C0.666761 0.5 0.253257 1.30836 0.670893 1.88705L3.29048 5.51685Z"
+                                        fill="#484A4F" fill-opacity="0.47" />
+                                </svg>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex items-center h-10 space-x-2">
+                        <div class="flex items-center h-full pl-3 space-x-2 border rounded">
+                            <div>
+                                <p class="text-sm uppercase font-mediumtext-gray-700">
+                                    MAD
+                                </p>
+                            </div>
+                            <div class="w-full h-full">
+                                <input type="text" class="w-full h-full text-sm font-medium text-gray-700 outline-none"
+                                    value="23.9">
+
+                            </div>
+                        </div>
+                        <div class="flex flex-col space-y-2">
+                            <div>
+                                <svg width="8" height="6" viewBox="0 0 8 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M3.29048 0.49755C3.63972 0.0136235 4.36028 0.0136232 4.70952 0.49755L7.32911 4.12735C7.74674 4.70604 7.33324 5.5144 6.61958 5.5144H1.38042C0.666761 5.5144 0.253257 4.70604 0.670893 4.12735L3.29048 0.49755Z"
+                                        fill="#484A4F" />
+                                </svg>
+
+                            </div>
+                            <div>
+                                <svg width="8" height="6" viewBox="0 0 8 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M3.29048 5.51685C3.63972 6.00078 4.36028 6.00078 4.70952 5.51685L7.32911 1.88706C7.74674 1.30836 7.33324 0.5 6.61958 0.5H1.38042C0.666761 0.5 0.253257 1.30836 0.670893 1.88705L3.29048 5.51685Z"
+                                        fill="#484A4F" fill-opacity="0.47" />
+                                </svg>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--split 2 -->
+                <div class="grid items-center grid-cols-4 mt-8 gap-x-12">
+                    <div>
+                        <p class="text-sm font-medium text-pricy-gray-200">Split 3</p>
+                    </div>
+                    <div class="relative">
+                        <select name=""
+                            class="w-full h-10 px-2 text-sm font-medium text-gray-700 border rounded outline-none appearance-none "
+                            id="">
+                            <option value="3 days">3 days</option>
+                        </select>
+                        <div class="absolute top-4 right-4">
+                            <svg width="8" height="6" viewBox="0 0 8 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M3.29048 5.51685C3.63972 6.00078 4.36028 6.00078 4.70952 5.51685L7.32911 1.88706C7.74674 1.30836 7.33324 0.5 6.61958 0.5H1.38042C0.666761 0.5 0.253257 1.30836 0.670893 1.88705L3.29048 5.51685Z"
+                                    fill="#484A4F" fill-opacity="0.47" />
+                            </svg>
+
+                        </div>
+                    </div>
+                    <div class="flex items-center h-10 space-x-2">
+                        <div class="flex items-center h-full pl-3 space-x-2 border rounded">
+                            <div>
+                                <p class="text-sm uppercase font-mediumtext-gray-700">
+                                    MAD
+                                </p>
+                            </div>
+                            <div class="w-full h-full">
+                                <input type="text" class="w-full h-full text-sm font-medium text-gray-700 outline-none"
+                                    value="23.9">
+
+                            </div>
+                        </div>
+                        <div class="flex flex-col space-y-2">
+                            <div>
+                                <svg width="8" height="6" viewBox="0 0 8 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M3.29048 0.49755C3.63972 0.0136235 4.36028 0.0136232 4.70952 0.49755L7.32911 4.12735C7.74674 4.70604 7.33324 5.5144 6.61958 5.5144H1.38042C0.666761 5.5144 0.253257 4.70604 0.670893 4.12735L3.29048 0.49755Z"
+                                        fill="#484A4F" />
+                                </svg>
+
+                            </div>
+                            <div>
+                                <svg width="8" height="6" viewBox="0 0 8 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M3.29048 5.51685C3.63972 6.00078 4.36028 6.00078 4.70952 5.51685L7.32911 1.88706C7.74674 1.30836 7.33324 0.5 6.61958 0.5H1.38042C0.666761 0.5 0.253257 1.30836 0.670893 1.88705L3.29048 5.51685Z"
+                                        fill="#484A4F" fill-opacity="0.47" />
+                                </svg>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex items-center h-10 space-x-2">
+                        <div class="flex items-center h-full pl-3 space-x-2 border rounded">
+                            <div>
+                                <p class="text-sm uppercase font-mediumtext-gray-700">
+                                    MAD
+                                </p>
+                            </div>
+                            <div class="w-full h-full">
+                                <input type="text" class="w-full h-full text-sm font-medium text-gray-700 outline-none"
+                                    value="23.9">
+
+                            </div>
+                        </div>
+                        <div class="flex flex-col space-y-2">
+                            <div>
+                                <svg width="8" height="6" viewBox="0 0 8 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M3.29048 0.49755C3.63972 0.0136235 4.36028 0.0136232 4.70952 0.49755L7.32911 4.12735C7.74674 4.70604 7.33324 5.5144 6.61958 5.5144H1.38042C0.666761 5.5144 0.253257 4.70604 0.670893 4.12735L3.29048 0.49755Z"
+                                        fill="#484A4F" />
+                                </svg>
+
+                            </div>
+                            <div>
+                                <svg width="8" height="6" viewBox="0 0 8 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M3.29048 5.51685C3.63972 6.00078 4.36028 6.00078 4.70952 5.51685L7.32911 1.88706C7.74674 1.30836 7.33324 0.5 6.61958 0.5H1.38042C0.666761 0.5 0.253257 1.30836 0.670893 1.88705L3.29048 5.51685Z"
+                                        fill="#484A4F" fill-opacity="0.47" />
+                                </svg>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-32 mt-12">
+                    <button class="w-full text-xs font-medium text-white bg-blue-600 rounded-lg h-11 ">+ Add
+                        Test</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,5 @@
 @section('title', 'Page Title')
 
 @section('content')
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, nostrum labore mollitia qui unde voluptatem quos
-        maiores recusandae quisquam id. Quia voluptate repellendus quasi doloribus, vero reiciendis dolor sequi hic. test
-    </p>
+
 @endsection
